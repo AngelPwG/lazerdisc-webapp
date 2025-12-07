b98b9d382fe6bd64a15d31d3451c2b552369b65c
NotPiwi <jose.angel.git@gmail.com>
2025-12-06 20:29:30 -0700

    Se inicializo la variable global db_connection

--- index.php
+++ index.php
@@ -1,30 +1,36 @@
 <?php
-    session_start();
+session_start();
 
-    // -- IMPORTANTE: Definimos esta constante para que DiscosController no falle
-    define('INDEX_KEY', true); 
+// -- IMPORTANTE: Definimos esta constante para que DiscosController no falle
+define('INDEX_KEY', true);
 
-    // 1. Controlador y acción por defecto
-    $nombre_controlador = isset($_GET['c']) ? $_GET['c'] : 'Auth';
-    $nombre_accion      = isset($_GET['a']) ? $_GET['a'] : 'index';
+// Incluimos la configuración de la BD
+require_once 'config/db_config.php';
 
-    // 2. LÓGICA DE SEGURIDAD
-    // Si la ruta solicitada NO es Auth Y el usuario NO tiene una sesión,
-    // lo enviamos al login.
-    if ($nombre_controlador !== 'Auth' && !isset($_SESSION['usuario'])) {
-        header("Location: index.php?c=Auth&a=index");
-        exit;
-    }
-    // -------------------------
+// Inicializamos la conexión global que usan los controladores
+$db_connection = getDBConnection();
 
-    $controlador_clase = $nombre_controlador . 'Controller';
+// 1. Controlador y acción por defecto
+$nombre_controlador = isset($_GET['c']) ? $_GET['c'] : 'Auth';
+$nombre_accion = isset($_GET['a']) ? $_GET['a'] : 'index';
 
-    // 3. Cargar controlador
-    require_once "controllers/$controlador_clase.php";
+// 2. LÓGICA DE SEGURIDAD
+// Si la ruta solicitada NO es Auth Y el usuario NO tiene una sesión,
+// lo enviamos al login.
+if ($nombre_controlador !== 'Auth' && !isset($_SESSION['usuario'])) {
+    header("Location: index.php?c=Auth&a=index");
+    exit;
+}
+// -------------------------
 
-    // 4. Instanciar
-    $controlador = new $controlador_clase();
+$controlador_clase = $nombre_controlador . 'Controller';
 
-    // 5. Ejecutar acción
-    $controlador->$nombre_accion();
+// 3. Cargar controlador
+require_once "controllers/$controlador_clase.php";
+
+// 4. Instanciar
+$controlador = new $controlador_clase();
+
+// 5. Ejecutar acción
+$controlador->$nombre_accion();
 ?>
